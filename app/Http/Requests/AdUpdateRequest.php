2f52ad4b3e032af8e5299adb6d1a9f156ab7474c
Cristian Cosano <cristiancosano@icloud.com>
2020-08-20 12:29:09 +0200

    Automatic commit

--- app/Http/Requests/AdUpdateRequest.php
+++ app/Http/Requests/AdUpdateRequest.php
@@ -35,7 +35,7 @@ class AdUpdateRequest extends FormRequest
             //'images.*' => 'image | mimes:jpg,jpeg,webp,png,JPG,JPEG,WEBP,PNG | max:5120',
             'fullName' => 'required',
             'email' => 'required|email',
-            'phone' => 'regex:/^\*?[0-9]{0,14}$/',
+            'phone' => 'regex:/^\+?[0-9]{0,14}$/',
         ];
     }
 
