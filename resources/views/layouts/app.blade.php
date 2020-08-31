c999ef97f1ccc4ae491eb31cef8edc3c6a6efb78
Cristian Cosano <cristiancosano@icloud.com>
2020-08-31 23:57:53 +0200

    Automatic commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title')  | @if(request()->routeIs('main')) {{config('settings.site_description')}} @endif{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') @if(!request()->routeIs('main')) | @endif{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
