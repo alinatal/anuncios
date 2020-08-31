e8de593bf5bffc10fb3b81a1676d3f7b5427c521
Cristian Cosano <cristiancosano@icloud.com>
2020-08-31 10:30:52 +0200

    Automatic commit

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -41,6 +41,7 @@
             selector: 'textarea',
             language: 'es_ES',
             language_url: '{{asset('vendor/tinymce/langs/es_ES.js')}}',
+            plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
         });
     </script>
 @endsection
