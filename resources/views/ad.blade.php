fbebe14cddd94ba3c6674056eeed65292ad6e5a4
Cristian Cosano <cristiancosano@icloud.com>
2020-09-04 07:51:24 +0200

    Automatic commit

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -5,7 +5,7 @@
     <meta property="og:url"                content="{{url()->current()}}" />
     <meta property="og:type"               content="website" />
     <meta property="og:title"              content="{{$ad->name}}" />
-    <meta property="og:description"        content="{{\Illuminate\Support\Str::words($ad->description, 20)}}" />
+    <meta property="og:description"        content="{{strip_tags(\Illuminate\Support\Str::words($ad->description, 20))}}" />
     <meta property="og:image"              content="{{secure_asset($images[0])}}" />
     <meta property="og:locale"              content="es_ES" />
 @endsection
